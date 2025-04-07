21377cd66e63499f12f925a12c97d4b36275cb81
Francesco0410 <54963972+Francesco0410@users.noreply.github.com>
2025-04-07 16:57:37 +0200

    Update esercizio_1_2.py

--- esercizio_1_2.py
+++ esercizio_1_2.py
@@ -4,10 +4,10 @@ if numero > 5:
     print("Il numero è maggiore di 5")
     if numero > 10:
         print("Hai scelto un grande numero")
-    else:
-        print("Hai scelto un numero enorme")
         if numero == 5:
             print("Il numero è uguale a 5")
+    else:
+        print("Hai scelto un numero enorme")
 else:
     print("Il numero è minore di 5")   
 
